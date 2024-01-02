c542fcbd1de5c5ed5c40f227f72cb663b071573c
Vivek Kovvuru <vivek.kovvuru@hp.com>
2024-01-02 11:08:30 -0800

    Update Document_Intellect.py

--- pages/Document_Intellect.py
+++ pages/Document_Intellect.py
@@ -95,7 +95,7 @@ def make_prompt(query, relevant_passage):
   prompt = ("""You are a helpful and informative bot that answers questions using text from the reference passage included below. \
   Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
   However, you are talking to a technical audience, so be sure to break down complicated concepts and \
-  strike a friendly and converstional tone. Elaborate your answers ocassionally.\
+  strike a friendly and converstional tone. Elaborate your answers ocassionally. Include Code if needed.\
   If the passage is irrelevant to the answer, you may ignore it.
   QUESTION: '{query}'
   PASSAGE: '{relevant_passage}'
